d4a135f56518738c14b33b4f6c198a6edc15a9b5
Dmitriy Dubson <ddubson@pivotal.io>
2017-08-22 13:40:31 -0400

    Basic mapping, reducing, filtering, composing examples

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # lets-haskell
+
+# Pre-requisites
+
+```
+brew install haskell-stack
+stack setup
+```
+
+# Compiling
+
+```
+stack buildstack
+stack exec lets-haskell
+```
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,24 @@
 module Main where
 
+{- Function f -}
+f x = x + 10
+
+{- Function g -}
+g x = x * x
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  {- Mapping example: add 10 to each item in list and print -}
+  print "Add 10 to each item in list and print"
+  print $ map (+ 10) [1, 2, 3]
+
+  {- Folding example: sum all elements in list, start with 0, and print -}
+  print "Sum all elements in list, start with 0, and print"
+  print $ foldr (+) 0 [1, 2, 3]
+
+  {- Filtering example: filter numbers that are greater than 3 from a list -}
+  print "Filter numbers that are > 3 from a list"
+  print $ filter (> 3) [1,2,3,4,5,6]
+
+  print "For each item 'x' in list, exec function f and then function g, i.e. g(f(x)), then print"
+  print $ map (g . f) [1,2,3]
